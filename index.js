411369132af1eccfec515c9335126ac406fc55ca
yosef langer <yosef@doar.systems>
2020-09-25 04:07:44 +0300

    added custom method

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var airports = require('./airports')
 var _ = require('lodash');
 var Autocomplete = require('triecomplete');
+var stringSimilarity = require('string-similarity');
 
 var airportIataAutocomplete = new Autocomplete();
-airportIataAutocomplete.initialize(_.map(airports, function(a) {
+airportIataAutocomplete.initialize(_.map(airports, function (a) {
   return [a.iata.toLowerCase(), a];
 }));
 
-airports = _.keyBy(airports, function(a) {
+airports = _.keyBy(airports, function (a) {
   return a.iata;
 });
 
-module.exports.lookupByIataCode = function(iataCode) {
+module.exports.lookupByIataCode = function (iataCode) {
   return airports[iataCode]
 }
 
-module.exports.searchByAirportName = function(name) {
+module.exports.searchByAirportName = function (name) {
   if (_.isEmpty(name)) {
     return [];
   }
@@ -36,8 +37,87 @@ module.exports.searchByAirportName = function(name) {
   var iatas = _.map(iataResults, 'iata');
 
   nameResults = _.chain(airports)
-    .filter(function(v) {
+    .filter(function (v) {
       return !_.includes(iatas, v.iata) && v.name.toLowerCase().indexOf(name) > -1
+    })
+    .value()
+
+  // have airports with matching iatas be listed before airports with names that
+  // have a matching substring
+  return iataResults.concat(nameResults);
+}
+
+module.exports.searchByAll = function (name) {
+  if (_.isEmpty(name)) {
+    return [];
+  }
+
+  name = name.toLowerCase().trim();
+  var nameWithNoSpecialChars = removeSpecialChars(name);
+  var iataResults = [];
+  var nameResults = [];
+
+  if (name.length <= 3) {
+    // searches airport by iata, using name as prefix
+    iataResults = _.chain(airportIataAutocomplete.search(name))
+      .map('value')
+      .sortBy('iata')
+      .value();
+  }
+
+  var iatas = _.map(iataResults, 'iata');
+
+  function removeSpecialChars(str) {
+    return str.replace(/-/g, " ").replace(/_/g, " ").replace(/[^\w\s]/gi, '');
+  }
+
+  var keysToCheck = [
+    'name',
+    'city',
+    'country',
+    'iata',
+  ];
+
+  nameResults = _.chain(airports)
+    .map(function (airport) {
+      var hits = 0;
+      for (var i in keysToCheck) {
+        var airportValue = airport[keysToCheck[i]];
+        if (airportValue) {
+          airportValue = airportValue.toLowerCase().trim();
+          var airportValueNoSpecialChars = removeSpecialChars(airportValue);
+          if (airportValue === name) {
+            hits += 1000;
+          } else if (airportValueNoSpecialChars === nameWithNoSpecialChars) {
+            hits += 800;
+          } else if (_.startsWith(airportValueNoSpecialChars, nameWithNoSpecialChars)
+          ) {
+            hits += 750;
+          } else if (_.startsWith(nameWithNoSpecialChars, airportValueNoSpecialChars)
+          ) {
+            hits += 600;
+          } else if (_.endsWith(nameWithNoSpecialChars, airportValueNoSpecialChars)
+          ) {
+            hits += 500;
+          } else if (_.endsWith(airportValueNoSpecialChars, nameWithNoSpecialChars)
+          ) {
+            hits += 500;
+          } else if (_.includes(nameWithNoSpecialChars, airportValueNoSpecialChars)
+          ) {
+            hits += 300;
+          } else if (_.includes(nameWithNoSpecialChars, airportValueNoSpecialChars)) {
+            hits += 250;
+          }
+        }
+      }
+      airport.hits = hits;
+      return airport;
+    })
+    .filter(function (airport) {
+      return airport.hits > 0;
+    })
+    .sort(function (airport1, airport2) {
+      return airport2.hits - airport1.hits;
     })
     .value()
 
